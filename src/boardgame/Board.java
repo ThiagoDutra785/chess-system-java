3e76d654439184f61c1e6b2df32020a57cff97b7
ThiagoDutra785 <thiagodutra785@gmail.com>
2021-07-22 18:30:33 -0300

    Little improvement in board priting and bug fix

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,13 +1,13 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		if(rows < 1 || columns < 1) {
+		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
@@ -18,27 +18,27 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	
+
 	public int getColumns() {
 		return columns;
 	}
-
+	
 	public Piece piece(int row, int column) {
-		if(!positionExists(row, column)) {
+		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
-
+	
 	public Piece piece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		if(theresIsAPiece(position)) {
+		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -46,20 +46,17 @@ public class Board {
 	}
 	
 	private boolean positionExists(int row, int column) {
-		return row >= 0 && row < rows && column >=0 && column < columns;
-	
-		
+		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 	
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
-		
 	}
 	
-	public boolean theresIsAPiece(Position position) {
-		if(!positionExists(position)) {
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
-		return piece(position)!= null;
+		return piece(position) != null;
 	}
 }
